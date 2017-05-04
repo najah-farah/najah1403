7480847fd725919f796f4d4aa5fb4ca00fe874bc
najah-farah <ubofarah7077@gmail.com>
2017-05-04 06:07:52 +0100

    LOGINn

--- db_conf.php
+++ db_conf.php
@@ -1,7 +1,7 @@
 <?php
 					//include("db_conf.php");
 //$con=mysqli_connect("127.0.0.1","root","","ethics_system","52967");
-$con=mysqli_connect("127.0.0.1:55994","root","96540301Aw","ethics_system");
+$con=mysqli_connect("127.0.0.1:55994","admin123","123","ethics_system");
 
 // Check connection
 if (mysqli_connect_errno()) {
